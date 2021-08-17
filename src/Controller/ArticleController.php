92cb2a93ef15cba643c1d066e2cb2d882a831a30
David Robert <contact.davidrobert@gmail.com>
2021-08-17 18:38:57 +0200

    back office sécurisé

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+
+    /**
+     * @Route("/articles", name="articles_list")
+     */
+    public function articleList(ArticleRepository $articleRepository)
+    {
+        $articles = $articleRepository->findBy(['isPublished' => true]);
+
+        return $this->render('articleList.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/articles/{id}", name="article_show")
+     */
+    public function articleShow($id, ArticleRepository $articleRepository)
+    {
+        $article = $articleRepository->find($id);
+
+        return $this->render('articleShow.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="article_search")
+     */
+    public function search(ArticleRepository $articleRepository, Request $request)
+    {
+        $term = $request->query->get('q');
+
+        $articles = $articleRepository->searchByTerm($term);
+
+        return $this->render('articleSearch.html.twig', [
+            'articles' => $articles,
+            'term' => $term
+        ]);
+    }
+
+
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotNull(message="Attention, le titre doit être rempli !")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="articles")
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isPublished;
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(?bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
